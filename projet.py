fde9b4fa4ff5089b83260bbbcd835d8588e890ca
Yasia <yasmine.gaouike@gmail.com>
2024-02-09 16:40:16 +0100

    maj online

---
+++ projet.py
@@ -0,0 +1,15 @@
+def sum(a,b):
+   return a+b
+
+def realDivi(a,b):
+   return a / b
+   else:
+        if a or b == 0:
+             print("ERROR you can't divide by 0 ;(")
+
+def sub(a,b):
+   return a - b
+
+def pow(a,b):
+   return a ** b
+
